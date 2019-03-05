5241c0c48990b73ede316130879a6f2ef720cedb
Wiandi Vreeswijk <wiavre@gmail.com>
2019-03-05 18:37:53 +0100

    Created and adapted the .java files  + Updated Gradle

--- app/src/main/java/com/example/geoguessswipe/MainActivity.java
+++ app/src/main/java/com/example/geoguessswipe/MainActivity.java
@@ -1,13 +1,146 @@
 package com.example.geoguessswipe;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerView.OnItemTouchListener {
+    List<GeoObject> mGeoObjects;
+    private GestureDetector mGestureDetector;
+    private int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGeoObjects = new ArrayList<>();
+
+        for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
+
+            mGeoObjects.add(new GeoObject(GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i]));
+        }
+
+        final RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+
+        mGeoRecyclerView.setLayoutManager(mLayoutManager);
+        mGeoRecyclerView.setHasFixedSize(true);
+        final GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        mGeoRecyclerView.setAdapter(mAdapter);
+        mGeoRecyclerView.addOnItemTouchListener(this);
+
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+                    // The first integer parameter refers to the dragging directions. We ignore these here.
+                    // The second integer parameter refers to the swiping directions.
+
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    //Called when a user swipes left or right on a ViewHolder
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+                        //Get the index corresponding to the selected position
+                        int position = (viewHolder.getAdapterPosition());
+                        mGeoObjects.remove(position);
+                        mAdapter.notifyItemRemoved(position);
+                        if(number++ <= 7){
+                            if(number == 0 || number == 3 || number == 5 || number == 6) correctMessage();
+                            else inCorrectMessage();
+                        }
+                        if(number == 7) finishedMessage();
+                    }
+
+                };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mGeoRecyclerView);
+
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback2 =
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+
+                    // The first integer parameter refers to the dragging directions. We ignore these here.
+                    // The second integer parameter refers to the swiping directions.
+
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    //Called when a user swipes left or right on a ViewHolder
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+                        //Get the index corresponding to the selected position
+                        int position = (viewHolder.getAdapterPosition());
+                        mGeoObjects.remove(position);
+                        mAdapter.notifyItemRemoved(position);
+                        if(number++ <= 7){
+                            if(number == 0 || number == 3 || number == 5 || number == 6) inCorrectMessage();
+                            else correctMessage();
+                        }
+                        if(number == 7) finishedMessage();
+                    }
+
+                };
+        ItemTouchHelper itemTouchHelper2 = new ItemTouchHelper(simpleItemTouchCallback2);
+        itemTouchHelper2.attachToRecyclerView(mGeoRecyclerView);
     }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View child = rv.findChildViewUnder(e.getX(), e.getY());
+        int mAdapterPosition = rv.getChildAdapterPosition(child);
+        if (child != null && mGestureDetector.onTouchEvent(e)) {
+            Toast.makeText(this, mGeoObjects.get(mAdapterPosition).getmGeoName(), Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean b) {
+
+    }
+    private void correctMessage(){
+        Toast.makeText(this,"correct" , Toast.LENGTH_SHORT).show();
+    }
+    private void inCorrectMessage(){
+        Toast.makeText(this,"incorrect" , Toast.LENGTH_SHORT).show();
+    }
+    private void finishedMessage(){
+        Toast.makeText(this,"you finished the game!" , Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
